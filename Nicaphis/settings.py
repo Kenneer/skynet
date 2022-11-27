d64df27cb7048b837251aa29457b49195d697c99
* <EneyderGutierrez>
2022-11-27 12:33:52 -0600

    first comdjit

--- Nicaphis/settings.py
+++ Nicaphis/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'Nicaphis.wsgi.application'
 #DATABASES = {
  #   'default': {
   #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+   # }
 #}
 
 #Database
